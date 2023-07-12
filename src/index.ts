1913def0e16f477cf509c75d75bc9696c659099c
ikoba <i.koba.jp@gmail.com>
2023-07-12 18:13:56 +0900

    Replace the worker with the correct one

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const worker = new Worker('ffprobe-wasm.worker.js');
+const worker = new Worker('ffprobe-worker.js');
+
+fetch("test.mp4")
+  .then(response => response.blob())
+  .then(blob => new File([blob], "test.mp4"))
+  .then(file => {
+    worker.postMessage(['get_file_info', file]);
+  })
+worker.onmessage = (e) => {
+  console.log(e.data)
+};
 console.log("Hello World!");
